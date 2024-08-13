516b67ef9aaa3d22dd29a65590dd180f3234c27b
Vitaliy S. Belov <vytalybelov@gmail.com>
2024-08-13 05:08:51 +0300

    params added

--- collect.py
+++ collect.py
@@ -2,44 +2,54 @@ import threading
 from icmplib import ping
 from getmac import get_mac_address
 import sqlite3
-import time
+from datetime import datetime
+import sys
 
-prefix = '192.168.0.'
+prefix = ''
 result = [None] * 254
 
 def pingHost(host):
-    address = prefix + str(host)
+    global prefix
+    address = prefix + '.' + str(host)
+    print(address)
     hostObject = ping(address, count=1, interval=1, timeout=2, id=None, source=None, family=None, privileged=True)
     if hostObject.is_alive:
         lock = threading.RLock()
         with lock:
+            global result
             result[host-1] = (address)
 
-
-
-if __name__ == "__main__":
+def main():
+    args = sys.argv[1:]
+    global prefix
+    prefix = args[0]
+    
     hosts = (i for i in range(1, 255))
     for host in hosts:
         thread = threading.Thread(target=pingHost, args=(host,))
         thread.start()
 
-con = sqlite3.connect("host_watcher.db")
-cur = con.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS activity(ping_time, address, mac)")
+    con = sqlite3.connect("host_watcher.db")
+    cur = con.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS activity(ping_time, address, mac)")
 
-rows = []
+    rows = []
 
-for i in range(1,255):
-    if result[i - 1]:
-        address = '192.168.8.' + str(i)
-        mac = get_mac_address(ip=address)
-        now = time.time()
-        row = tuple([str(now), str(address), str(mac)])
-        rows.append(row)
+    for i in range(1,255):
+        if result[i - 1]:
+            address = prefix + '.' + str(i)
+            mac = get_mac_address(ip=address)
+            now = datetime.now()
+            fmt_now = now.strftime("%Y-%m-%d %H:%M:%S")
+            row = tuple([fmt_now, str(address), str(mac)])
+            rows.append(row)
 
 
-print(rows)
-sql = 'INSERT INTO activity VALUES (?, ?, ?)'
-cur.executemany(sql, rows)
-con.commit()
-con.close()
+    print(rows)
+    sql = 'INSERT INTO activity VALUES (?, ?, ?)'
+    cur.executemany(sql, rows)
+    con.commit()
+    con.close()
+
+if __name__ == "__main__":
+    main()
